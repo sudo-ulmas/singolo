6842cadae3b3076029ec59c9e9c24c0570b28412
ulmasbek <[rakhmatullaevulmas@gmail.com]>
2020-03-21 21:00:54 +0500

    feature: add screen toggle to phones

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const MENU = document.getElementById('menu');
 
 let lishki = MENU.querySelectorAll('a');
 let sections = document.querySelectorAll('.wrapper > section');
+let vbScreen = document.getElementById('black_vertical');
+let hbScreen = document.getElementById('black_horizontal');
 
 
 
@@ -15,11 +17,33 @@ document.addEventListener('scroll', () => {
     const curPos = window.scrollY;
     sections.forEach((el, index) => {
         if (curPos >= el.offsetTop - 80 && (el.offsetTop - 80 + el.offsetHeight > curPos)) {
-            console.log(el.offsetTop + "  " + curPos + "  " + el.offsetHeight);
             lishki.forEach(el => el.classList.remove('current'));
             lishki[index].classList.add('current');
         }
     });
 });
 
-document.get
+document.getElementsByClassName('slider__phone_ver')[0].addEventListener('click',(event)=>{
+   if(vbScreen.style.zIndex==0){
+       vbScreen.style.zIndex = '1';
+   }
+   else{
+       vbScreen.style.zIndex = '0';
+   }
+});
+vbScreen.addEventListener('click',(event)=>{
+    vbScreen.style.zIndex='0';
+});
+
+document.getElementsByClassName('slider__phone_gor')[0].addEventListener('click', (event) => {
+    
+    if (hbScreen.style.zIndex == 0) {
+        hbScreen.style.zIndex = '1';
+    }
+    else {
+        hbScreen.style.zIndex = '0';
+    }
+});
+hbScreen.addEventListener('click', (event) => {
+    hbScreen.style.zIndex = '0';
+});
